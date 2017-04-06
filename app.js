05d34a916ec13e148d018337abd47fc4843f1b25
Faras Momin <famomin@utexas.edu>
2017-04-06 10:25:52 -0500

    navigator for the app

---
+++ app.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Navigator
+} from 'react-native';
+
+// Importing scenes for our app
+import Home from './Home';
+import NewGame from './NewGame';
+import Splash from './Splash';
+
+class GameCAH extends Component {
+
+  // Our renderScene function will choose which scene to render based on the route id
+  renderScene(route, navigator) {
+    if (route.id === 1) {
+      return <Home navigator={navigator} />
+    }
+    else if (route.id === 2) {
+      return <NewGame navigator={navigator} />
+    }
+    else if (route.id === 3) {
+      return <Splash navigator={navigator} />
+    }
+  }
+
+  // The configureScene function allows us to change the animation properties of a scene
+  configureScene() {
+    return Navigator.SceneConfigs.FloatFromBottom;
+  }
+
+  render() {
+    return (
+      // Our Navigator handles the transition between different scenes in our app
+      <Navigator
+        // The first page we are going to render
+        initialRoute={{ id: 3 }}
+        // Passing in our renderScene function
+        renderScene={this.renderScene}
+        // Passing in our configureScene function
+        configureScene={this.configureScene}
+      />
+    );
+  }
+
+}
+
+AppRegistry.registerComponent('GameCAH', () => GameCAH);
