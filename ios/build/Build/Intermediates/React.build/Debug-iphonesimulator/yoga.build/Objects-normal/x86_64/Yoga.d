7d606e670b9ed92b610e9180c80560451a99c720
megan liken <meganliken@megans-MacBook-Pro-2.local>
2017-04-06 20:26:34 -0500

    random push

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
@@ -1,6 +1,2 @@
 dependencies: \
-  /Users/AzimMomin/Desktop/CodingBootCamp/Project-3/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c \
-  /Users/AzimMomin/Desktop/CodingBootCamp/Project-3/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
-  /Users/AzimMomin/Desktop/CodingBootCamp/Project-3/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
-  /Users/AzimMomin/Desktop/CodingBootCamp/Project-3/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
-  /Users/AzimMomin/Desktop/CodingBootCamp/Project-3/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
+  /Users/meganliken/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c
