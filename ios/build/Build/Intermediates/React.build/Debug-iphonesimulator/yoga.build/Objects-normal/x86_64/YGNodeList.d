f205f30391487ee749ddaf9bcd9c0ecc81fc6bcb
Faras Momin <famomin@utexas.edu>
2017-04-08 00:54:56 -0500

    ios folder

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
@@ -1,11 +1,6 @@
 dependencies: \
-<<<<<<< HEAD
   /Users/FarasMomin/Documents/Coding\ Bootcamp/React\ Native\ Apps/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
   /Users/FarasMomin/Documents/Coding\ Bootcamp/React\ Native\ Apps/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
   /Users/FarasMomin/Documents/Coding\ Bootcamp/React\ Native\ Apps/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
   /Users/FarasMomin/Documents/Coding\ Bootcamp/React\ Native\ Apps/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
   /Users/FarasMomin/Documents/Coding\ Bootcamp/React\ Native\ Apps/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
-=======
-  /Users/meganliken/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
-  /Users/meganliken/GameCAH/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h
->>>>>>> 7d606e670b9ed92b610e9180c80560451a99c720
