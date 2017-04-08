1f82defaf5254ee494cfa2e72f66b8ee89654eb8
Faras Momin <famomin@utexas.edu>
2017-04-08 13:20:36 -0500

    format chanage for cards and added submit button

--- src/components/SliderEntry.js
+++ src/components/SliderEntry.js
@@ -19,33 +19,27 @@ export default class SliderEntry extends Component {
         ) : false;
 
         return (
-          <View style={{
-              flex: 1,
-              flexDirection: 'column',
-              justifyContent: 'space-between',
-          }}>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-            <View>
-              <TouchableOpacity
-                activeOpacity={0.7}
-                style={styles.slideInnerContainer}
-                onPress={() => { alert(`You've clicked '${title}'`); }}
-                >
-                  {/*<View style={[styles.imageContainer, even ? styles.imageContainerEven : {}]}>
-                      <Image
-                        source={{ uri: illustration }}
-                        style={styles.image}
-                      />
-                      <View style={[styles.radiusMask, even ? styles.radiusMaskEven : {}]} />
-                  </View>*/}
-                  <View style={[styles.textContainer, even ? styles.textContainerEven : {}]}>
-                      {/*{ uppercaseTitle }*/}
-                      <Text style={[styles.subtitle, even ? styles.subtitleEven : {}]} numberOfLines={10}>{ uppercaseTitle }</Text>
-                  </View>
-              </TouchableOpacity>
-            </View>
-          </View>
+
+
+              <View>
+                <TouchableOpacity
+                  activeOpacity={0.7}
+                  style={styles.slideInnerContainer}
+                  // onPress={() => { alert(`You've clicked '${title}'`); }}
+                  >
+                    {/*<View style={[styles.imageContainer, even ? styles.imageContainerEven : {}]}>
+                        <Image
+                          source={{ uri: illustration }}
+                          style={styles.image}
+                        />
+                        <View style={[styles.radiusMask, even ? styles.radiusMaskEven : {}]} />
+                    </View>*/}
+                    <View style={[styles.textContainer, even ? styles.textContainerEven : {}]}>
+                        {/*{ uppercaseTitle }*/}
+                        <Text style={[styles.subtitle, even ? styles.subtitleEven : {}]} numberOfLines={10}>{ uppercaseTitle }</Text>
+                    </View>
+                </TouchableOpacity>
+              </View>
         );
     }
 }
