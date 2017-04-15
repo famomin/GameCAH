ae5a8839ece9f2d0e92bef8c6cb915dac19640e4
Faras Momin <famomin@utexas.edu>
2017-04-15 18:00:56 -0500

    firebase and new alert for join game

--- Home.js
+++ Home.js
@@ -5,11 +5,33 @@
  */
 
  import React, { Component } from 'react';
- import { AppRegistry, StyleSheet, Modal, Image, Platform, Navigator, TouchableHighlight } from 'react-native';
+ import { AppRegistry, StyleSheet, Modal, Image, Platform, Navigator, TouchableHighlight, AlertIOS, ListView } from 'react-native';
  import { Spinner, Text, View, Content, Container, Header, Title, Button, Icon, InputGroup, ListItem, List, Radio, CheckBox, Thumbnail, Card, CardItem, H3, Form, Item, Input } from 'native-base';
  import { Col, Row, Grid } from 'react-native-easy-grid';
+ import * as firebase from 'firebase';
+
+ const config = {
+     apiKey: "AIzaSyCTKP5CD183HRaUy3NnS13iHAQNFJab8oo",
+     authDomain: "project3database.firebaseapp.com",
+     databaseURL: "https://project3database.firebaseio.com",
+     storageBucket: "project3database.appspot.com",
+   };
+   const firebaseApp = firebase.initializeApp(config);
 
  export default class Home extends Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
    // The press function takes in an argument (movie name) and pushes to the navigator the individual movie page
    // It also passes along the name of the movie which will be used fetch information from the OMDB API
    press(Page) {
@@ -68,34 +90,12 @@
               </Row>
 
               <Row>
-                 <Button block danger onPress={() => this.press('JoinGame')}>
+                 <Button block danger onPress={this._addItem.bind(this)}>
                      <Text>Join Existing Game</Text>
                  </Button>
               </Row>
 
-              {/* <Row>
-                  <Button onPress={() => this.press()}>
-                     <Text>Page Get Code</Text>
-                  </Button>
-              </Row>
 
-              <Row>
-                  <Button onPress={() => this.press()}>
-                     <Text>Page Enter Code</Text>
-                 </Button>
-              </Row> */}
-
-              {/* <Row>
-                  <Button onPress={() => this.press('AllPlayers')}>
-                     <Text>All Players</Text>
-                  </Button>
-              </Row> */}
-
-              {/* <Row>
-                  <Button onPress={() => this.press('GamePlay')}>
-                     <Text>Final Page</Text>
-                 </Button>
-              </Row> */}
 
               <Row>
                   <Button onPress={() => this.press('CardSlider')}>
@@ -108,7 +108,50 @@
         </Image>
        );
      }
+
+      componentDidMount() {
+         this.listenForItems(this.itemsRef);
+      }
+
+     listenForItems(itemsRef) {
+        itemsRef.on('value', (snap) => {
+
+          // get children as an array
+          var items = [];
+          snap.forEach((child) => {
+            items.push({
+              title: child.val().title,
+              _key: child.key
+            });
+          });
+
+          this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(items)
+          });
+
+        });
+      }
+
+     _addItem() {
+       AlertIOS.prompt(
+         'Enter Your Name',
+         null,
+         [
+           {text:'Cancel', onPress: () => console.log('Cancel Pressed'), style:'Cancel'},
+           {
+             text: 'Enter',
+             onPress: (text) => {
+               this.itemRef.push({title: text})
+             }
+           },
+         ],
+         'plain-text'
+       );
+     }
    }
+
+
+
 const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
--- app.js
+++ app.js
@@ -9,15 +9,15 @@ import {
   AppRegistry,
   Navigator
 } from 'react-native';
-import * as firebase from 'firebase';
-
-var config = {
-    apiKey: "AIzaSyCTKP5CD183HRaUy3NnS13iHAQNFJab8oo",
-    authDomain: "project3database.firebaseapp.com",
-    databaseURL: "https://project3database.firebaseio.com",
-    storageBucket: "project3database.appspot.com",
-  };
-  firebase.initializeApp(config);
+// import * as firebase from 'firebase';
+//
+// var config = {
+//     apiKey: "AIzaSyCTKP5CD183HRaUy3NnS13iHAQNFJab8oo",
+//     authDomain: "project3database.firebaseapp.com",
+//     databaseURL: "https://project3database.firebaseio.com",
+//     storageBucket: "project3database.appspot.com",
+//   };
+//   const firebaseApp = firebase.initializeApp(config);
 
 
 // Importing scenes for our app
